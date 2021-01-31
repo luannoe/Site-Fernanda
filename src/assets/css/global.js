f54c8e0c9078dac6670bd31d1e673f74647cb8a9
Luan Noé <luan.noe@gmail.com>
2021-01-31 20:19:54 -0300

    Update

--- src/assets/css/global.js
+++ src/assets/css/global.js
@@ -1,5 +1,9 @@
 import { css } from '@emotion/core';
 
-export const CustomGlobal = css``;
+export const CustomGlobal = css`
+  html {
+    scroll-behavior: smooth;
+  }
+`;
 
 export default CustomGlobal;
